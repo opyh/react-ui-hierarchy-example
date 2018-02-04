a9f880212e59830be2955f7ba35b55ab9eaf9a89
Sebastian Felix Zappe <hypo@me.com>
2018-02-04 01:21:40 +0100

    First working version using npm package

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
 // @flow
 
 import * as React from 'react';
-import UIHierarchy from './components/UIHierarchy';
+import UIHierarchy from 'react-ui-hierarchy';
 import Colors from './Colors';
 
 import './App.css';
 
 type State = {
-  level: number,
+  levels: number,
 };
 
 class App extends React.Component<{}, State> {
   state = {
-    level: 1,
+    levels: 1,
   };
 
   render() {
     return (
       <div className="App">
         <nav>
-          <h1>Tabs go here</h1>
+          <h1>
+            <a href='https://github.com/opyh/react-ui-hierarchy'><code>react-ui-hierarchy</code></a> – a responsive UI hierarchy system.
+          </h1>
         </nav>
 
         <UIHierarchy animationDuration={500}>
-          {Array.apply(null, Array(this.state.level))
-              .map((el, index) => {
-                return (<section className='view' key={index} style={{ backgroundColor: Colors.background[index % Colors.background.length] }}>
-                  {index === 0 ? null : <button onClick={() => this.pop()}>Back</button>}
-                  <section>Level {index + 1}</section>
-                  <button onClick={() => this.push()}>Deeper!</button>
-                </section>);
-              })}
+          {Array.apply(null, Array(this.state.levels))
+            .map((el, index) => {
+              return (<section className='view' key={index} style={{ backgroundColor: Colors.background[index % Colors.background.length] }}>
+                {index === 0 ? null : <button onClick={() => this.pop()}>Back</button>}
+                <section>Level {index}</section>
+                <button onClick={() => this.push()}>See more</button>
+              </section>);
+            })}
         </UIHierarchy>
       </div>
     );
   }
 
   push() {
-    this.setState({ level: this.state.level + 1 });
+    this.setState({ levels: this.state.levels + 1 });
   }
 
   pop() {
     if (this.state.level === 1) return;
-    this.setState({ level: this.state.level - 1 });
+    this.setState({ levels: this.state.levels - 1 });
   }
 }
 
